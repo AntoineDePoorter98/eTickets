a8f0e7d7dc2a2c000c0b5c2dcf45955ed1c26e08
Antoine De Poorter <antoine.depoorter@outlook.com>
2022-07-05 10:22:08 +0200

    setup models / db

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var qerviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = qerviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+}
